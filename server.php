56a362bdb6ca281acc0bff6d018c78aef9e36085
ulyana <sm.ulyana@mail.ru>
2020-05-03 16:38:01 +0300

    takind damage

--- server.php
+++ server.php
@@ -1,8 +1,10 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/User.php';
+require_once __DIR__ . '/Utilities.php';
 
 use WebSocketGame\User;
+use WebSocketGame\Utilities;
 use Workerman\Timer;
 use Workerman\Worker;
 
@@ -10,7 +12,7 @@ $users = [];
 
 $ws_worker = new Worker("websocket://0.0.0.0:8000");
 $ws_worker->onWorkerStart = function () use (&$users) {
-    Timer::add(0.1, function() use (&$users){
+    Timer::add(0.1, function() use   (&$users){
         $data = ["type" => "users", "data" => $users];
         foreach ($users as $user) {
             $user->getConnection()->send(json_encode($data));
@@ -31,13 +33,25 @@ $ws_worker->onWorkerStart = function () use (&$users) {
 //    $inner_tcp_worker->listen();
 };
 
-
-
 $ws_worker->onMessage = function ($connection, $data) use (&$users) {
     $data = json_decode($data, true);
     if ($data["type"]==="move") {
         foreach ($users as $user) {
             if ($user->getConnection() === $connection) {
+                $user->move($data["data"]["horizontal"]*5,$data["data"]["vertical"]*5);
+            }
+        }
+    }
+    if ($data["type"]==="melee") {
+        foreach ($users as $attacking) {
+            if ($attacking->getConnection() === $connection) {
+                if ($attacking->meleeRadiusCheck($data["data"]["x"],$data["data"]["y"])) {
+                        foreach ($users as $attacked) {
+                            if ($attacked->radiusCheck($data["data"]["x"],$data["data"]["y"])) {
+                                $attacked->takindDamage($attacking->generationDamage());
+                            }
+                    }
+                }
                 $user->move($data["data"]["horizontal"]*5,$data["data"]["vertical"]*5);
             }
         }
